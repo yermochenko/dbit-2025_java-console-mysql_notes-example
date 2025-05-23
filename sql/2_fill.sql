a5dffd8bc9003162d2fe406bcfd1cf8052713444
Sergey Yermochenko <yermochenko@gmail.com>
2025-05-23 22:32:22 +0300

    Structure of database developed

---
+++ sql/2_fill.sql
@@ -0,0 +1,17 @@
+INSERT INTO note
+(id, title                                                                                                    , date        , done ) VALUES
+(1 , 'Attend lectures on the discipline "Big Data Processing"'                                                , '2025-03-15', TRUE ),
+(2 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-03-17', TRUE ),
+(3 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-03-18', TRUE ),
+(4 , 'Pass the exam on the discipline "Big Data Processing"'                                                  , '2025-03-22', FALSE),
+(5 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-07', TRUE ),
+(6 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-08', TRUE ),
+(7 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-14', FALSE),
+(8 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-15', TRUE ),
+(9 , 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-21', TRUE ),
+(10, 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-22', FALSE),
+(11, 'Attend lectures on the discipline "Development and Analysis of the Software Requirements"'              , '2024-04-30', TRUE ),
+(12, 'Prepare and bring to the Department of Applied and System Programming documents for production practice', '2024-06-27', FALSE),
+(13, 'Take part in the conference to opening production practice'                                             , '2024-06-30', FALSE),
+(14, 'Suggest and report to the Department of Applied and System Programming the topic of your graduate work' , '2024-07-01', FALSE),
+(15, 'Pass the exam on the discipline "Development and Analysis of the Software Requirements"'                , '2024-07-28', FALSE);
